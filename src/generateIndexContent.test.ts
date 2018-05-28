ca4cb2c3decad4c64b762f690bc708aa49ff0217
kingdaro <kingdaro@gmail.com>
2018-05-28 17:29:07 -0400

    fix tests for generateIndexContent

--- src/generateIndexContent.test.ts
+++ src/generateIndexContent.test.ts
@@ -1,4 +1,3 @@
-import * as assert from "assert"
 import { generateIndexContent } from "./generateIndexContent"
 
 describe("generateIndexContent", () => {
@@ -6,8 +5,7 @@ describe("generateIndexContent", () => {
     const files = ["a.ts", "b.ts", "c.ts"]
     const content = generateIndexContent(files)
 
-    assert.equal(
-      content,
+    expect(content).toBe(
       `export * from "./a"\nexport * from "./b"\nexport * from "./c"\n`,
     )
   })
@@ -15,13 +13,13 @@ describe("generateIndexContent", () => {
   it("returns empty string if given no files", () => {
     const content = generateIndexContent([])
 
-    assert.equal(content, "")
+    expect(content).toBe("")
   })
 
   it("excludes files that match the given patterns", () => {
     const files = ["file.test.ts", "__snapshots__", "a.ts"]
     const content = generateIndexContent(files, [".test.", "__snapshots__"])
 
-    assert.equal(content, `export * from "./a"\n`)
+    expect(content).toBe(`export * from "./a"\n`)
   })
 })
